4e1e014e2b98d8252f70708696955525c8371c60
lazycatchen <13844221197@126.com>
2018-10-10 16:45:38 +0800

    Signed-off-by: lazycatchen <13844221197@126.com>

--- figure_timepill.py
+++ figure_timepill.py
@@ -4,7 +4,22 @@ import re
 data = pd.read_csv("result.csv")
 data = data.fillna(0)
 data = data.drop(columns=['Unnamed: 0'])
-time=[]
+time,time_rusult=[],[]
 for i in range(len(data)):
     timestr=data.loc[i].date
-    time.append(re.findall(r"\d+\.?\d*",timestr))
+    time_num=re.findall(r"\d+\.?\d*",timestr)
+    try:
+         num1=int(time_num[0])
+         num2=int(time_num[1])
+         if 'a' in timestr:
+             if num1==12:
+                 time=num2
+             else:
+                 time=num1*60+num2
+  #       elif timestr[7:9]=='pm':
+         elif 'p' in timestr:
+            time=(num1+12)*60+num2
+    except:
+        print('异常')
+    time_rusult.append(time)
+time_rusult.to_csv("timeresult.csv",encoding="utf_8_sig")
